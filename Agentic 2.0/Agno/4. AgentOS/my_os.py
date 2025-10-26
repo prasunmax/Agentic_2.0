10b5fbf42fd0a350b5cbde1954b8c2ed119dc11c
Mayank Aggarwal <31075014+mayank953@users.noreply.github.com>
2025-10-26 09:30:58 +0530

    input and output

--- Agentic 2.0/Agno/4. AgentOS/my_os.py
+++ Agentic 2.0/Agno/4. AgentOS/my_os.py
@@ -1,18 +1,23 @@
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.os import AgentOS
+from agno.db.sqlite import SqliteDb
 
 import os
 
 # Set your API key (replace YOUR_API_KEY with your actual key)
 os.environ["GOOGLE_API_KEY"] = "AIzaSyBQ_pZ37jQiQG1tFGiyfViNrWXOZXjew5U"
 
+db = SqliteDb(db_file="tmp/data.db")
 
 assistant = Agent(
     name="Assistant",
     model=Gemini(id="gemini-2.0-flash"),
     instructions=["You are a helpful AI assistant."],
     markdown=True,
+    db=db,
+    add_history_to_context=True,
+    num_history_runs=3,
 )
 
 agent_os = AgentOS(
